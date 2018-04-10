19e59a4788dc853bdf8046259b4d4c97c450a1b2
Yang Chongyuan <yangchongyuan@renrenche.com>
2018-04-10 19:39:48 +0800

    update

--- src/main/java/org/casper/learning/io/nettyrpc/client/RpcSyncUtil.java
+++ src/main/java/org/casper/learning/io/nettyrpc/client/RpcSyncUtil.java
@@ -24,6 +24,7 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
         public void acquire() {
             super.acquire(done);
         }
+
         public void release() {
             super.release(done);
         }
